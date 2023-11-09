864a7c1d5ef532f8bf03c22f173de2ff91f7ea85
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-11-10 08:20:23 +0900

    db

--- server/server/src/routers/sign_up_router.ts
+++ server/server/src/routers/sign_up_router.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response, Router } from "express";
 import { UserDatabase } from "../database/user_repository";
 import { QueryChecker } from "../util/query_checker";
 import { respRest } from "../rest/rest_producer";
+import { PrivilegeEnum } from "../util/user_privilege";
+import { Role } from "../util/role";
+import { parse } from "uuid";
 
 const signUpRouter: Router = require('express').Router();
 const userDatabase = new UserDatabase();
@@ -15,20 +18,21 @@ signUpRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
     let name: string = req.body.name;
     let nickname: string = req.body.nickname;
     let birthday: string = req.body.birthday;
-    let privilege: number = req.body.privilege;
-    
     let checker = new QueryChecker();
-    if (privilege >= 0 && checker.notNull(key, id, password, identifyCode, email, name, nickname, privilege)) {
-        if (checker.hasInvalidString(name, password)) {
+    if (checker.notNull(key, id, password, identifyCode, email, name, nickname)) {
+        if (checker.hasInvalidString(id, password, identifyCode, email, name, nickname, birthday)) {
             res.status(400).send("Invalid characters in name or password");
         }
         else {
+            let privilege = PrivilegeEnum.DEFAULT;
+            let role = Role.STUDENT;
+            let penalty = 0;
+            let classNumber = parseInt(key.toString().split("")[1]);
+            let studentNumber = parseInt(key.toString().substring(2));      
             userDatabase.signUp(key, id, password, identifyCode, email, name, nickname, birthday, privilege).then((result: boolean) => {
                 if (result) {
                     req.session.key = key;
                     req.session.id = id;
-                    req.session.password = password;
-                    req.session.identifyCode = identifyCode;
                     req.session.email = email;
                     req.session.name = name;
                     req.session.nickname = nickname;
