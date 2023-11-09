f93f463ad1188740b8404d298067e08ae6bda429
thislife_won <sw8744@naver.com>
2023-11-09 17:57:27 +0900

    Edit login and signUp

--- server/server/src/routers/login_router.ts
+++ server/server/src/routers/login_router.ts
@@ -6,20 +6,18 @@ import { respRest } from "../rest/rest_producer";
 const loginRouter: Router = require('express').Router();
 const userDatabase = new UserDatabase();
 loginRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-    let key: number = req.body.key;
-    let name: string = req.body.name;
+    let id: string = req.body.id;
     let password: string = req.body.password;
     let checker = new QueryChecker();
-    if (key && checker.notNull(key, name, password)) {
-        if (checker.hasInvalidString(name, password)) {
+    if (checker.notNull(id, password)) {
+        if (checker.hasInvalidString(id, password)) {
             res.status(400).send("Invalid characters in name or password");
         } else {
-            userDatabase.login(key, name, password).then((result: boolean) => {
+            userDatabase.login(id, password).then((result: boolean) => {
                 if (result) {
-                    req.session.key = key;
-                    req.session.name = name;
+                    req.session.id = id;
                     req.session.password = password;
-                    userDatabase.getPrivilege(key).then((privilege: number) => {
+                    userDatabase.getPrivilege(id).then((privilege: number) => {
                         req.session.privilege = privilege;
                         req.session.save(() => console.log("Session saved"));
                         res.status(200).send(respRest(200, 0));
--- server/server/src/routers/sign_up_router.ts
+++ server/server/src/routers/sign_up_router.ts
@@ -8,20 +8,31 @@ const userDatabase = new UserDatabase();
 
 signUpRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
     let key: number = req.body.key;
-    let name: string = req.body.name;
-    let privilege: number = req.body.privilege;
+    let id: string = req.body.id;
     let password: string = req.body.password;
+    let identifyCode: string = req.body.identifyCode;
+    let email: string = req.body.email;
+    let name: string = req.body.name;
+    let nickname: string = req.body.nickname;
+    let birthday: string = req.body.birthday;
+    let privilege: number = req.body.privilege;
+    
     let checker = new QueryChecker();
-    if (privilege >= 0 && checker.notNull(key, name, password)) {
+    if (privilege >= 0 && checker.notNull(key, id, password, identifyCode, email, name, nickname, privilege)) {
         if (checker.hasInvalidString(name, password)) {
             res.status(400).send("Invalid characters in name or password");
         }
         else {
-            userDatabase.signUp(key, name, privilege, password).then((result: boolean) => {
+            userDatabase.signUp(key, id, password, identifyCode, email, name, nickname, birthday, privilege).then((result: boolean) => {
                 if (result) {
                     req.session.key = key;
-                    req.session.name = name;
+                    req.session.id = id;
                     req.session.password = password;
+                    req.session.identifyCode = identifyCode;
+                    req.session.email = email;
+                    req.session.name = name;
+                    req.session.nickname = nickname;
+                    req.session.birthday = birthday;
                     req.session.privilege = privilege;
                     req.session.save(() => console.log("Session saved"));
                     res.status(200).send(respRest(200, 0));
