b23cdc146c7fd3b5a49628409bc442de02cd223c
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-11-13 19:17:37 +0900

    Update date_aligner.ts

--- server/server/src/util/date_aligner.ts
+++ server/server/src/util/date_aligner.ts
@@ -1,11 +1,9 @@
 export class dateAligner {
-    dateAlign(...params: object[]) {
-        let result = params;
-        result.sort(function (x: any, y: any) {
-            const a = x['date'];
-            const b = y['date'];
+    sortByDate(...params: Post[]) {
+        return params.sort(function (x: Post, y: Post) {
+            const a = x.getCreatedAt();
+            const b = y.getCreatedAt();
             return a - b;
         })
-        return result;
     }
 }
