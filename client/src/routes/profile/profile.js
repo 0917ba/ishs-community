d48ff44bff5190f99ff8d89c4a036de15fe9f960
ag4710 <leun4710@gmail.com>
2023-12-24 17:21:59 +0900

    Mypage appearance complete v2

--- client/src/routes/profile/profile.js
+++ client/src/routes/profile/profile.js
@@ -1,42 +1,42 @@
-// import styles from './profile.module.css';
-// import { useEffect } from 'react';
+import styles from './profile.module.css';
+import { useEffect } from 'react';
 
-// function Profile() {
-//   const [loginState, setLoginState] = useState(false);
-//   useEffect(() => {
-//     (async () => {
-//       const formData = {
-//         mgitethod: 'GET',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           withCredentials: true,
-//         },
-//         body: JSON.stringify({}),
-//       };
-//       const serverUrl = process.env.REACT_APP_SERVER_URL;
-//       res = await fetch(`serverUrl` + `/checksession`, formData);
-//     })();
-//     if (res.status === 200) {
-//       setLoginState(true);
-//     }
-//     if (res.status === 400) {
-//       setLoginState(false);
-//     }
-//   }, []);
+function Profile() {
+  const [loginState, setLoginState] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const formData = {
+        mgitethod: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          withCredentials: true,
+        },
+        body: JSON.stringify({}),
+      };
+      const serverUrl = process.env.REACT_APP_SERVER_URL;
+      res = await fetch(`serverUrl` + `/checksession`, formData);
+    })();
+    if (res.status === 200) {
+      setLoginState(true);
+    }
+    if (res.status === 400) {
+      setLoginState(false);
+    }
+  }, []);
 
-//   if (loginState === false) {
-//     return (
-//       <div>
-//         <h1>다음에 다시 도전해보세요!</h1>
-//       </div>
-//     );
-//   } else {
-//     return (
-//       <div>
-//         <h1>프로필</h1>
-//       </div>
-//     );
-//   }
-// }
+  if (loginState === false) {
+    return (
+      <div>
+        <h1>다음에 다시 도전해보세요!</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>프로필</h1>
+      </div>
+    );
+  }
+}
 
-// export default Profile;
+export default Profile;
--- client/src/routes/signin/findPW.js
+++ client/src/routes/signin/findPW.js
@@ -1,33 +1,33 @@
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-// function FindPw() {
-//   useEffect(() => {
-//     (async () => {
-//       const formData = {
-//         mgitethod: 'GET',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           withCredentials: true,
-//         },
-//         body: JSON.stringify({}),
-//       };
-//       const serverUrl = process.env.REACT_APP_SERVER_URL;
-//       res = await fetch(`serverUrl` + `/check_session`, formData);
-//     })();
-//   }, []);
-//   const navigate = useNavigate();
+function FindPw() {
+  useEffect(() => {
+    (async () => {
+      const formData = {
+        mgitethod: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          withCredentials: true,
+        },
+        body: JSON.stringify({}),
+      };
+      const serverUrl = process.env.REACT_APP_SERVER_URL;
+      res = await fetch(`serverUrl` + `/check_session`, formData);
+    })();
+  }, []);
+  const navigate = useNavigate();
 
-//   const onClickMain = () => {
-//     navigate('/main');
-//   };
+  const onClickMain = () => {
+    navigate('/main');
+  };
 
-//   return (
-//     <div>
-//       <button onClick={onClickMain}>홈화면으로 가기</button>
+  return (
+    <div>
+      <button onClick={onClickMain}>홈화면으로 가기</button>
 
-//       <h1>아직 구현되지 않은 기능입니다.</h1>
-//     </div>
-//   );
-// }
+      <h1>아직 구현되지 않은 기능입니다.</h1>
+    </div>
+  );
+}
 
-// export default FindPw;
+export default FindPw;
