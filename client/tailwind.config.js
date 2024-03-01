558f2aa866af5a36928b336bdd4485620bded0d8
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2024-03-02 01:41:01 +0900

    add mainpage

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+const config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
--- server/server/src/main.ts
+++ server/server/src/main.ts
@@ -83,7 +83,7 @@ app.use('*', (req: Request, res: Response, next: NextFunction) => {
 app.use('/signup', require('./routers/sign_up_router'));
 app.use('/signin', require('./routers/sign_in_router'));
 app.use('/check_session', require('./routers/check_session_router'));
-app.use('/logout', require('./routers/logout_router'));
+app.use('/signout', require('./routers/logout_router'));
 app.use('/user', require('./routers/userInfo_router'));
 app.use('/post', require('./routers/post_router'));
 app.use('/comment', require('./routers/comment_router'));
