cdcb9118c7fa7e2502bf33497b534963dabb9197
ag4710 <leun4710@gmail.com>
2024-03-07 20:21:18 +0900

    UserDemerit design emend v1

--- client/src/component/mypage/user/demerit/UserDemerit.jsx
+++ client/src/component/mypage/user/demerit/UserDemerit.jsx
@@ -1,14 +1,24 @@
 import styles from "../../MyPage.module.css";
+import styled from "styled-components";
 import ModaluserDemerit from "../../modal/ModalUserDemerit";
 
 export default function UserDemerit(props) {
     return (
       <div>
         <ModaluserDemerit />
-        <u2 className={styles.DemeritGuide}>
+        <Title>
           현재 누적 벌점은 {props.userDemerit}점
-        </u2>
+        </Title>
         <h1></h1>
       </div>
     );
   }
+
+const Title = styled.div`
+  position: relative;
+  font-size: 3vmin;
+  padding-bottom: 0.9vh;
+  width: 95%;
+  bottom: 3vh;
+  border-bottom: 0.1vh solid  #c0c0c0;
+`;
