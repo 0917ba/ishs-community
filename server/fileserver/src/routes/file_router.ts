e9cac6997496efbc061cd9c1d66817e2111c168e
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2024-03-10 23:56:36 +0900

    finish image upload server

--- server/fileserver/src/routes/file_router.ts
+++ server/fileserver/src/routes/file_router.ts
@@ -1,47 +1,49 @@
 import { Request, Response, NextFunction } from "express";
 import fs from "fs";
+import path from "path";
 import { respRest } from "../rest/rest_producer";
 
 const fileRouter = require('express').Router();
 
 fileRouter.get('/image', (req: Request, res: Response, next: NextFunction) => {
     let id = req.query.id;
-    let index = req.query.index;
-    let file = findFileByName(id + "_" + index);
-    if (fs.existsSync("../../uploads/img/" + file)) {
-        res.sendFile("../../uploads/img/" + file);
+    if (id == undefined || id == null) res.sendStatus(400).send(respRest(400, "Bad request"));
+    let file = findFileByName(String(id), "image");
+    if (fs.existsSync("./uploads/image/" + file)) {
+        res.sendFile(path.join(__dirname + "../../../uploads/image/" + file));
     } else {
         res.sendStatus(404).send(respRest(404, "File not found"));
     }
 });
 
-fileRouter.get('/video', (req: Request, res: Response, next: NextFunction) => {
-    let id = req.query.id;
-    let index = req.query.index;
-    let file = findFileByName(id + "_" + index);
-    if (fs.existsSync("../../uploads/video/" + file)) {
-        res.sendFile("../../uploads/video/" + file);
-    } else {
-        res.sendStatus(404).send(respRest(404, "File not found"));
-    }
-});
+// fileRouter.get('/video', (req: Request, res: Response, next: NextFunction) => {
+//     let id = req.query.id;
+//     let index = req.query.index;
+//     let file = findFileByName(id + "_" + index);
+//     if (fs.existsSync("../../uploads/video/" + file)) {
+//         res.sendFile("../../uploads/video/" + file);
+//     } else {
+//         res.sendStatus(404).send(respRest(404, "File not found"));
+//     }
+// });
 
-fileRouter.get('/audio', (req: Request, res: Response, next: NextFunction) => {
-    let id = req.query.id;
-    let index = req.query.index;
-    let file = findFileByName(id + "_" + index);
-    if (fs.existsSync("../../uploads/audio/" + file)) {
-        res.sendFile("../../uploads/audio/" + file);
-    } else {
-        res.sendStatus(404).send(respRest(404, "File not found"));
-    }
-});
+// fileRouter.get('/audio', (req: Request, res: Response, next: NextFunction) => {
+//     let id = req.query.id;
+//     let index = req.query.index;
+//     let file = findFileByName(id + "_" + index);
+//     if (fs.existsSync("../../uploads/audio/" + file)) {
+//         res.sendFile("../../uploads/audio/" + file);
+//     } else {
+//         res.sendStatus(404).send(respRest(404, "File not found"));
+//     }
+// });
 
-const findFileByName = (name: string) => {
-    let files = fs.readdirSync("../../uploads/img/");
+const findFileByName = (name: string, type: string) => {
+    let files = fs.readdirSync(`./uploads/${type}/`);
     let file = files.find((file) => {
         return file.includes(name);
     });
+    console.log(file);
     return file;
 }
 
