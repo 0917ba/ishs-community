15b0a9bac938726b7ddfc77bab0a4ae3a0dd4f8c
rlaqhrud <rlaqhrud0209@naver.com>
2024-03-11 20:40:19 +0900

    naneunbabo

--- client/src/component/page/Navigation.jsx
+++ client/src/component/page/Navigation.jsx
@@ -5,11 +5,11 @@ export default function Navigation() {
         <a href='http://i-science.icehs.kr/sub/info.do?m=0101&s=isciencehs'>
           학교 소개
         </a>
-        <a href='#'>지식 in곽</a>
+        <a href='/preparing'>지식 in곽</a>
         <a href='http://i-science.icehs.kr/schdList.do?m=0304&s=isciencehs'>
           학교 일정
         </a>
-        <a href='#'>학습 자료</a>
+        <a href='/preparing'>학습 자료</a>
         <a href='http://ionya.cc'>면불 신청</a>
         <a href='/BigBang'>빅뱅</a>
       </div>
